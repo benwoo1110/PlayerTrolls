74b465e6e06c9a0ad5e335b79b23588e2db6132a
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-03-21 14:24:15 +0800

    Simplify flag with new flag creation tool.

--- src/main/java/dev/benergy10/playertrolls/utils/TrollFlags.java
+++ src/main/java/dev/benergy10/playertrolls/utils/TrollFlags.java
@@ -1,110 +1,45 @@
 package dev.benergy10.playertrolls.utils;
 
 import dev.benergy10.minecrafttools.commands.flags.Flag;
-import dev.benergy10.minecrafttools.commands.flags.FlagParseFailedException;
-import dev.benergy10.minecrafttools.commands.flags.NoValueFlag;
-import dev.benergy10.minecrafttools.commands.flags.RequiredValueFlag;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import dev.benergy10.minecrafttools.commands.flags.FlagCreatorTool;
 
 public class TrollFlags {
 
-    public static final Flag<Integer> DURATION = new RequiredValueFlag<Integer>("Duration", "--duration", Integer.class) {
-        @Override
-        public Collection<String> suggestValue() {
-            return IntStream.range(1, 21).boxed().map(Object::toString).collect(Collectors.toList());
-        }
+    public static final Flag<Integer> DURATION = new FlagCreatorTool
+            .NumberCreator<>("Duration", "--duration", Integer.class)
+            .parser(Integer::parseInt)
+            .defaultValue(5)
+            .range(-1, Integer.MAX_VALUE)
+            .create();
 
-        @Override
-        public Integer getValue(@NotNull String input) {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                throw new FlagParseFailedException("%s is not a number!", input);
-            }
-        }
+    public static final Flag<Boolean> DO_DAMAGE = new FlagCreatorTool
+            .BooleanCreator("DoDamage", "--do-damage")
+            .presentValue(true)
+            .create();
 
-        @Override
-        public Integer getDefaultValue() {
-            return 5;
-        }
-    };
+    public static final Flag<Integer> INTERVAL = new FlagCreatorTool
+            .NumberCreator<>("Interval", "--interval", Integer.class)
+            .parser(Integer::parseInt)
+            .defaultValue(5)
+            .range(1, 1000)
+            .create();
 
-    public static final Flag<Boolean> DO_DAMAGE = new NoValueFlag<Boolean>("DoDamage", "--do-damage", Boolean.class) {
-        @Override
-        public Boolean getValue() {
-            return true;
-        }
+    public static final Flag<Integer> REPEAT = new FlagCreatorTool
+            .NumberCreator<>("Repeat", "--repeat", Integer.class)
+            .parser(Integer::parseInt)
+            .defaultValue(0)
+            .range(0, Integer.MAX_VALUE)
+            .create();
 
-        @Override
-        public Boolean getDefaultValue() {
-            return false;
-        }
-    };
+    public static final Flag<Integer> INTENSITY = new FlagCreatorTool
+            .NumberCreator<>("Intensity", "--intensity", Integer.class)
+            .parser(Integer::parseInt)
+            .defaultValue(2)
+            .range(1, 1000)
+            .create();
 
-    public static final Flag<Integer> INTERVAL = new RequiredValueFlag<Integer>("Interval", "--interval", Integer.class) {
-        @Override
-        public Collection<String> suggestValue() {
-            return IntStream.range(1, 21).boxed().map(Object::toString).collect(Collectors.toList());
-        }
-
-        @Override
-        public Integer getValue(@NotNull String input) {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                throw new FlagParseFailedException("%s is not a number!", input);
-            }
-        }
-
-        @Override
-        public Integer getDefaultValue() {
-            return 2;
-        }
-    };
-
-    public static final Flag<Integer> REPEAT = new RequiredValueFlag<Integer>("Repeat", "--repeat", Integer.class) {
-        @Override
-        public Collection<String> suggestValue() {
-            return IntStream.range(1, 21).boxed().map(Object::toString).collect(Collectors.toList());
-        }
-
-        @Override
-        public Integer getValue(@NotNull String input) {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                throw new FlagParseFailedException("%s is not a number!", input);
-            }
-        }
-
-        @Override
-        public Integer getDefaultValue() {
-            return 0;
-        }
-    };
-
-    public static final Flag<Integer> INTENSITY = new RequiredValueFlag<Integer>("Intensity", "--intensity", Integer.class) {
-        @Override
-        public Collection<String> suggestValue() {
-            return IntStream.range(1, 21).boxed().map(Object::toString).collect(Collectors.toList());
-        }
-
-        @Override
-        public Integer getValue(@NotNull String input) {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                throw new FlagParseFailedException("%s is not a number!", input);
-            }
-        }
-
-        @Override
-        public Integer getDefaultValue() {
-            return 2;
-        }
-    };
+    public static final Flag<Boolean> EXPLODE_BLOCKS = new FlagCreatorTool
+            .BooleanCreator("ExplodeBlocks", "--explode-blocks")
+            .presentValue(true)
+            .create();
 }
